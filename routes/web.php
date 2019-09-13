bf08ffb843adbf3456d168d28243543a48818914
ice <1355871239@qq.com>
2019-09-13 05:34:45 +0000

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,15 @@
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
-
+/*
+HTTP 请求 URL 动作  作用
+GET /login  SessionsController@create   显示登录页面
+POST    /login  SessionsController@store    创建新会话（登录）
+DELETE  /logout SessionsController@destroy  销毁会话（退出登录）
+*/
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
